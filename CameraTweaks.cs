014edb3cd47a6e75e88cd792acab251a3e1ef81b
searica <143636061+searica@users.noreply.github.com>
2023-11-12 20:47:25 -0800

    Initial version

---
+++ CameraTweaks.cs
@@ -0,0 +1,187 @@
+﻿// Ignore Spelling: CameraTweaks Jotunn
+
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+using CameraTweaks.Configs;
+using CameraTweaks.Extensions;
+using System.Reflection;
+using UnityEngine;
+
+namespace CameraTweaks
+{
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+    internal class CameraTweaks : BaseUnityPlugin
+    {
+        internal const string Author = "Searica";
+        public const string PluginName = "CameraTweaks";
+        public const string PluginGUID = $"{Author}.Valheim.{PluginName}";
+        public const string PluginVersion = "0.0.1";
+
+        internal static ConfigEntry<float> MaxDistance;
+        internal static ConfigEntry<float> MaxDistanceBoat;
+
+        private static readonly string MainSection = ConfigManager.SetStringPriority("Global", 1);
+        private static readonly string CameraSection = "Camera";
+
+        public void Awake()
+        {
+            Log.Init(Logger);
+
+            ConfigManager.Init(PluginGUID, Config, false);
+
+            Log.Verbosity = ConfigManager.BindConfig(
+                MainSection,
+                "Verbosity",
+                LogLevel.Low,
+                "Low will log basic information about the mod. Medium will log information that " +
+                "is useful for troubleshooting. High will log a lot of information, do not set " +
+                "it to this without good reason as it will slow Down your game."
+            );
+
+            MaxDistance = ConfigManager.BindConfig(
+                CameraSection,
+                "MaxDistance",
+                6f,
+                "Maximum distance you can zoom out to. Vanilla default is 6.",
+                new AcceptableValueRange<float>(6f, 20f)
+            );
+
+            MaxDistanceBoat = ConfigManager.BindConfig(
+                CameraSection,
+                "MaxDistanceBoat",
+                12f,
+                "Maximum distance you can zoom out to while in a boat. Vanilla default is 6.",
+                new AcceptableValueRange<float>(6f, 20f)
+            );
+
+            MaxDistance.SettingChanged += delegate
+            {
+                if (GameCamera.instance != null)
+                {
+                    GameCamera.instance.m_maxDistance = MaxDistance.Value;
+                }
+            };
+
+            MaxDistance.SettingChanged += delegate
+            {
+                if (GameCamera.instance != null)
+                {
+                    GameCamera.instance.m_maxDistanceBoat = MaxDistanceBoat.Value;
+                }
+            };
+
+            ConfigManager.Save();
+            ConfigManager.SaveOnConfigSet(true);
+
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), harmonyInstanceId: PluginGUID);
+            Game.isModded = true;
+
+            ConfigManager.SetupWatcher();
+        }
+
+        public void OnDestroy()
+        {
+            ConfigManager.Save();
+        }
+    }
+
+    [HarmonyPatch(typeof(GameCamera))]
+    internal class GameCameraPatch
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(GameCamera.Awake))]
+        private static void GameCameraAwakePostfix(GameCamera __instance)
+        {
+            __instance.m_maxDistance = CameraTweaks.MaxDistance.Value;
+            __instance.m_maxDistanceBoat = CameraTweaks.MaxDistanceBoat.Value;
+        }
+    }
+
+    /// <summary>
+    ///     Log level to control output to BepInEx log
+    /// </summary>
+    internal enum LogLevel
+    {
+        Low = 0,
+        Medium = 1,
+        High = 2,
+    }
+
+    /// <summary>
+    ///     Helper class for properly logging from static contexts.
+    /// </summary>
+    internal static class Log
+    {
+        #region Verbosity
+
+        internal static ConfigEntry<LogLevel> Verbosity { get; set; }
+        internal static LogLevel VerbosityLevel => Verbosity.Value;
+
+        #endregion Verbosity
+
+        internal static ManualLogSource _logSource;
+
+        internal static void Init(ManualLogSource logSource)
+        {
+            _logSource = logSource;
+        }
+
+        internal static void LogDebug(object data) => _logSource.LogDebug(data);
+
+        internal static void LogError(object data) => _logSource.LogError(data);
+
+        internal static void LogFatal(object data) => _logSource.LogFatal(data);
+
+        internal static void LogInfo(object data, LogLevel level = LogLevel.Low)
+        {
+            if (VerbosityLevel >= level)
+            {
+                _logSource.LogInfo(data);
+            }
+        }
+
+        internal static void LogMessage(object data) => _logSource.LogMessage(data);
+
+        internal static void LogWarning(object data) => _logSource.LogWarning(data);
+
+        internal static void LogGameObject(GameObject prefab, bool includeChildren = false)
+        {
+            LogInfo("***** " + prefab.name + " *****");
+            foreach (Component compo in prefab.GetComponents<Component>())
+            {
+                LogComponent(compo);
+            }
+
+            if (!includeChildren) { return; }
+
+            LogInfo("***** " + prefab.name + " (children) *****");
+            foreach (Transform child in prefab.transform)
+            {
+                LogInfo($" - {child.gameObject.name}");
+                foreach (Component compo in child.gameObject.GetComponents<Component>())
+                {
+                    LogComponent(compo);
+                }
+            }
+        }
+
+        internal static void LogComponent(Component compo)
+        {
+            LogInfo($"--- {compo.GetType().Name}: {compo.name} ---");
+
+            PropertyInfo[] properties = compo.GetType().GetProperties(ReflectionUtils.AllBindings);
+            foreach (var property in properties)
+            {
+                LogInfo($" - {property.Name} = {property.GetValue(compo)}");
+            }
+
+            FieldInfo[] fields = compo.GetType().GetFields(ReflectionUtils.AllBindings);
+            foreach (var field in fields)
+            {
+                LogInfo($" - {field.Name} = {field.GetValue(compo)}");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,66 @@
+# CameraTweaks
+Client-side mod that lets you set the maximum distance you can zoom out to when on foot or in a boat.
+
+## Instructions
+If you are using a mod manager for Thunderstore simply install the mod from there. If you are not using a mod manager then, you need a modded instance of Valheim with BepInEx installed and to put the dll file for this mod into the plugins folder of BepInEx.
+
+## Configuration
+Changes made to the configuration settings will be reflected in-game immediately (no restart required). The mod also has a built in file watcher so you can edit settings via an in-game configuration manager (changes applied upon closing the in-game configuration manager) or by changing values in the file via a text editor or mod manager.
+
+### Global Section:
+
+**Verbosity**
+- Low will log basic information about the mod. Medium will log information that is useful for troubleshooting. High will log a lot of information, do not set it to this without good reason as it will slow down your game.
+  - Acceptable values: Low, Medium, High
+  - Default value: Low.
+
+### Camera Section
+
+**MaxDistance**
+- Maximum distance you can zoom out to. Vanilla default is 6.
+    - Default value: 6
+
+**MaxDistanceBoat**
+- Maximum distance you can zoom out to while in a boat. Vanilla default is 6.
+    - Default value: 12
+
+## Known Issues
+None so far, tell me if you find any.
+
+## Donations/Tips
+ My mods will always be free to use but if you feel like saying thanks you can tip/donate.
+
+  [![ko-fi](https://ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/searica)
+
+## Source Code
+Github: [Link]
+
+### Contributions
+If you would like to provide suggestions, make feature requests, or reports bugs and compatibility issues you can either open an issue on the Github repository or tag me (@searica) with a message on my discord [Searica's Mods](https://discord.gg/sFmGTBYN6n).
+<!--the [Jotunn discord](https://discord.gg/DdUt6g7gyA), or the [Odin Plus discord](https://discord.gg/mbkPcvu9ax)-->
+
+I'm a grad student and have a lot of personal responsibilities on top of that so I can't promise I will respond quickly, but I do intend to maintain and improve the mod in my free time.
+
+### Credits
+This mod
+
+#### Development Credits
+- Huge shoutout and thanks to the developers of Jotunn for all their work making the library and to Margmas specifically for all their help and advice.
+- Thanks
+
+#### Community Credits
+- Thanks to
+
+## Shameless Self Plug (Other Mods By Me)
+If you like this mod you might like some of my other ones.
+
+#### Building Mods
+- [More Vanilla Build Prefabs](https://valheim.thunderstore.io/package/Searica/More_Vanilla_Build_Prefabs/)
+- [Extra Snap Points Made Easy](https://valheim.thunderstore.io/package/Searica/Extra_Snap_Points_Made_Easy/)
+- [BuildRestrictionTweaksSync](https://valheim.thunderstore.io/package/Searica/BuildRestrictionTweaksSync/)
+
+#### Gameplay Mods
+- [FortifySkillsRedux](https://valheim.thunderstore.io/package/Searica/FortifySkillsRedux/)
+- [DodgeShortcut](https://valheim.thunderstore.io/package/Searica/DodgeShortcut/)
+- [ProjectileTweaks](https://github.com/searica/ProjectileTweaks/)
+- [SkilledCarryWeight](https://github.com/searica/SkilledCarryWeight/)
